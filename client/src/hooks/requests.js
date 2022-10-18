9aadaf2766019c376f078369eb6e4a3526f0a20b
Sergio Salinas Junior <104912672+SergioSalinasJunior@users.noreply.github.com>
2022-10-18 15:15:25 -0300

    Add planets route
    -Update client side hooks for getAllPlanets function
    -Implement planets controller and router in the same folder

--- client/src/hooks/requests.js
+++ client/src/hooks/requests.js
@@ -1,6 +1,10 @@
+const API_URL = 'http://localhost:5000/planets';
+
 async function httpGetPlanets() {
   // TODO: Once API is ready.
-  // Load planets and return as JSON.
+    // Load planets and return as JSON.
+  const response = await fetch(`${API_URL}/planets`);
+  return await response.json();
 }
 
 async function httpGetLaunches() {
--- server/src/app.js
+++ server/src/app.js
@@ -1,8 +1,11 @@
 const express = require('express');
+const planetsRouter = require('./routes/planets/planets.router');
 
 //fancy listener function for our built in node http server
 const app = express();
 
+//chains of middleware that handles req as they come
 app.use(express.json());
+app.use(planetsRouter);
 
 module.exports = app;
