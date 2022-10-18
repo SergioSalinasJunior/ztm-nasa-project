840fd139e3bd0ed308fa04314bd4233d75da3562
Sergio Salinas Junior <104912672+SergioSalinasJunior@users.noreply.github.com>
2022-10-18 18:22:28 -0300

    Build and deploy project
    -Add script to deploy project into production ready status
    -Use react-build module for production optmized version

--- server/src/app.js
+++ server/src/app.js
@@ -1,6 +1,8 @@
 const express = require('express');
-const planetsRouter = require('./routes/planets/planets.router');
+const path = require('path');
 const cors = require('cors');
+
+const planetsRouter = require('./routes/planets/planets.router');
 
 //fancy listener function for our built in node http server
 const app = express();
@@ -9,7 +11,13 @@ const app = express();
 app.use(cors({
     origin: 'http://localhost:3000',
 }));
+
 app.use(express.json());
+app.use(express.static(path.join(__dirname, '..', 'public')));
+
 app.use(planetsRouter);
+app.get('/', (_req, res) => {
+    res.sendFile(path.join(__dirname, '..', 'public', 'index.html'))
+})
 
 module.exports = app;
