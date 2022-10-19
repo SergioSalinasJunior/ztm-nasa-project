ba41761f84f2c74d314dd4e7dbef4cd616a8f5ce
Sergio Salinas Junior <104912672+SergioSalinasJunior@users.noreply.github.com>
2022-10-19 13:24:59 -0300

    Add new launch validation
    -Add validation for required fields
    -Add validation for data field input

--- server/src/routes/launches/launches.controller.js
+++ server/src/routes/launches/launches.controller.js
@@ -9,7 +9,22 @@ function httpGetAllLaunches(_req, res) {
 
 function httpAddNewLaunch(req,res) {
     const launch = req.body;
+
+    //validates if all needed inputs are not null/false
+    if (!launch.mission || !launch.rocket || !launch.launchDate || !launch,destination) {
+        return res.status(400).json({
+            error: 'Missing required launch property',
+        });
+    }
+
     launch.launchDate = new Date(launch.launchDate);
+    //if (launch.launchDate.toString() === 'Invalid Date') { ==another method
+    if (isNaN(launch.launchDate)) {
+        return res.status(400).json({
+            error: 'Missing required launch property',
+        });
+    }
+
     addNewLaunch(launch);
     return res.status(201).json(launch);
 }
