05ae7c33622df6cae21c74902e063e58e0a594b7
Sergio Salinas Junior <104912672+SergioSalinasJunior@users.noreply.github.com>
2022-10-19 14:40:14 -0300

    Integrate new launch with front end
    -Correct errors in launch code flow
    -Correct property misnamed target
    -Add httpSubmitLaunch method with client side changes

--- client/src/hooks/requests.js
+++ client/src/hooks/requests.js
@@ -11,7 +11,7 @@ async function httpGetLaunches() {
   // TODO: Once API is ready.
   // Load launches, sort by flight number, and return as JSON.
   const response = await fetch(`${API_URL}/launches`);
-  const fetchedLaunches =  await response.json();
+  const fetchedLaunches = await response.json();
   return fetchedLaunches.sort((a, b) => {
     return a.flightNumber - b.flightNumber;
   });
@@ -20,6 +20,19 @@ async function httpGetLaunches() {
 async function httpSubmitLaunch(launch) {
   // TODO: Once API is ready.
   // Submit given launch data to launch system.
+  try {
+    return await fetch(`${API_URL}/launches`, {
+      method: "post",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(launch),
+    });
+  } catch(err) {
+    return {
+      ok: false,
+    };
+  }
 }
 
 async function httpAbortLaunch(id) {
--- server/src/routes/launches/launches.controller.js
+++ server/src/routes/launches/launches.controller.js
@@ -1,35 +1,29 @@
 const {
     getAllLaunches,
     addNewLaunch,
-} = require('../../models/launches.model');
-
-function httpGetAllLaunches(_req, res) {
-    return res.status(200).json(getAllLaunches);
-}
-
-function httpAddNewLaunch(req,res) {
+  } = require('../../models/launches.model');
+  function httpGetAllLaunches(req, res) {
+    return res.status(200).json(getAllLaunches());
+  }
+  function httpAddNewLaunch(req, res) {
     const launch = req.body;
 
-    //validates if all needed inputs are not null/false
-    if (!launch.mission || !launch.rocket || !launch.launchDate || !launch,destination) {
+    if (!launch.mission || !launch.rocket || !launch.launchDate
+      || !launch.target) {
         return res.status(400).json({
-            error: 'Missing required launch property',
+          error: 'Missing required launch property',
         });
-    }
-
+      }
     launch.launchDate = new Date(launch.launchDate);
-    //if (launch.launchDate.toString() === 'Invalid Date') { ==another method
     if (isNaN(launch.launchDate)) {
-        return res.status(400).json({
-            error: 'Missing required launch property',
-        });
+      return res.status(400).json({
+        error: 'Invalid launch date',
+      });
     }
-
     addNewLaunch(launch);
     return res.status(201).json(launch);
-}
-
-module.exports = {
+  }
+  module.exports = {
     httpGetAllLaunches,
     httpAddNewLaunch,
-};
+  };
