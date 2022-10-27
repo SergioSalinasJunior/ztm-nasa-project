8cd5f61f6e97a3abc0b135aaa94828db32a44d3b
Sergio Salinas Junior <104912672+SergioSalinasJunior@users.noreply.github.com>
2022-10-27 13:57:51 -0300

    Find and save planets all together
    -Create documents
    -Add savePlanet function
    -Implement async behaviour

--- server/src/models/planets.model.js
+++ server/src/models/planets.model.js
@@ -4,9 +4,7 @@ const path = require('path');
 
 //third-part import
 const {parse} = require('csv-parse');
-
-//array to hold the values read from the kepler_data
-const habitablePlanets = [];
+const planets = require('./planets.mongo');
 
 /*
     This function filters planets that are possible habitable planets based earth attributes found on the article:
@@ -39,24 +37,39 @@ function loadPlanetsData () {
             comment: '#',
             columns: true
         }))
-        .on('data', (data) => {
+        .on('data', async (data) => {
             if (isHabitablePlanet(data)) {
-                habitablePlanets.push(data);
+                savePlanet(data);
             }
         })
         .on('error', (err) => {
             console.log(err);
             reject(err);
         })
-        .on('end', () => {
-            console.log(`${habitablePlanets.length} habitable planets found!`);
+        .on('end', async () => {
+            const countPlanetsFound = (await getAllPlanets()).length;
+            console.log(`${countPlanetsFound} habitable planets found!`);
             resolve();
         });
     });
 }
 
-function getAllPlanets() {
-    return habitablePlanets;
+async function getAllPlanets() {
+    return await planets.find({});
+}
+
+async function savePlanet(planet) {
+    try {
+    await planets.updateOne({
+        keplerName: planet.kepler_name,
+    }, {
+        keplerName: planet.kepler_name,
+    }, {
+        upsert: true,
+    });
+    } catch(err) {
+    console.error(`Could not save planet ${err}`);
+    }
 }
 
 module.exports = {
