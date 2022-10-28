c5123eabac8e3b6c386aa57058cbb84520210da2
Sergio Salinas Junior <104912672+SergioSalinasJunior@users.noreply.github.com>
2022-10-28 13:37:41 -0300

    Use findOneAndUpdate
    -Update to prevent leaking of MongoDB information in the API response

--- server/src/models/launches.model.js
+++ server/src/models/launches.model.js
@@ -52,7 +52,7 @@ async function saveLaunch(launch) {
         throw new Error('No matching planet found');
     }
 
-    await launchesDatabase.updateOne({
+    await launchesDatabase.findOneAndUpdate({
         flightNumber: launch.flightNumber,
         }, launch, {
         upsert: true,
