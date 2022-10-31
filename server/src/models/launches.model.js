47cf0e7ba12480dea42244ae60dbe725c222ac15
Sergio Salinas Junior <104912672+SergioSalinasJunior@users.noreply.github.com>
2022-10-31 17:32:58 -0300

    Populate launches if not already loaded

--- server/src/models/launches.model.js
+++ server/src/models/launches.model.js
@@ -20,11 +20,12 @@ saveLaunch(launch);
 
 const SPACEX_API_URL = 'https://api.spacexdata.com/v4/launches/query';
 
-async function loadLaunchesData() {
+async function populateLaunches() {
   console.log('Downloading launch data...');
   const response = await axios.post(SPACEX_API_URL, {
     query: {},
     options: {
+        agination: false,
       populate: [
         {
           path: 'rocket',
@@ -61,8 +62,25 @@ for (const launchDoc of launchDocs) {
   };
 
   console.log(`${launch.flightNumber} ${launch.mission}`);
+  //TODO: populate launches collection...
 }
 
+async function loadLaunchesData() {
+    const firstLaunch = await findLaunch({
+      flightNumber: 1,
+      rocket: 'Falcon 1',
+      mission: 'FalconSat',
+    });
+    if (firstLaunch) {
+      console.log('Launch data already loaded!');
+    } else {
+      await populateLaunches();
+    }
+  }
+
+  async function findLaunch(filter) {
+    return await launchesDatabase.findOne(filter);
+  }
 //Not used anymore when migrated to a database
 //launches.set(launch.flightNumber, launch);
 
@@ -71,7 +89,7 @@ for (const launchDoc of launchDocs) {
 //}
 
 async function existsLaunchWithId(launchId) {
-    return await launchesDatabase.findOne({
+    return await findLaunch({
         flightNumber: launchId,
     });
 }
